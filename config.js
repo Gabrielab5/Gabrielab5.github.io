91badf7c9403562631f41bc06d28bee384e590a0
Gabriela <gabriela@gmail.com>
2025-07-26 15:10:46 +0300

    fixed messaging

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const EMAILJS_CONFIG = {
-  PUBLIC_KEY: 'wUYTzH_ABC123xyz',
-  SERVICE_ID: 'service_9wxyz5f',
-  TEMPLATE_ID: 'template_h1a2b3c',
+  PUBLIC_KEY: 'yzlDBiUW1zDE6a7SU',
+  SERVICE_ID: 'service_pyvswzw',
+  TEMPLATE_ID: 'template_e0w03h2',
 };
